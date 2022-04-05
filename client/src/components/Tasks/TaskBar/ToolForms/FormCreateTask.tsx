f5dc24a75e5dea90bc999ec9a5eca99ab07cef99
vojtech.tesar <vojtech.tesar@dcsoft.cz>
2022-04-05 18:18:09 +0200

    Add validation

--- client/src/components/Tasks/TaskBar/ToolForms/FormCreateTask.tsx
+++ client/src/components/Tasks/TaskBar/ToolForms/FormCreateTask.tsx
@@ -10,6 +10,8 @@ import { faCalendarPlus } from "@fortawesome/free-solid-svg-icons";
 import DatetimePicker from "../../../DatetimePicker/DatetimePicker";
 import { Task } from "../../TaskList/TaskList";
 import StarsInput from "../../../StarsInput/StarsInput";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 import "./ToolForms.scss";
 
@@ -20,6 +22,39 @@ type FormData = {
   priority: number;
 };
 
+const schema = yup
+  .object({
+    title: yup
+      .string()
+      .required((value) => `The ${value.path} field is required.`)
+      .min(
+        3,
+        (value) =>
+          `The ${value.path} must be at least ${value.value} characters.`
+      )
+      .max(
+        200,
+        (value) =>
+          `The ${value.path} may not be greater than ${value.max} characters.`
+      ),
+    desc: yup
+      .string()
+      .max(
+        2000,
+        (value) =>
+          `The ${value.path} may not be greater than ${value.max} characters.`
+      ),
+    due: yup
+      .date()
+      .required((value) => `The ${value.path} field is required.`)
+      .min(
+        new Date(Date.now() + 1000 * 60),
+        (value) => `The ${value.path} must be in the past.`
+      )
+      .typeError((value) => `The ${value.path} is not a valid date.`),
+  })
+  .required();
+
 export interface Props {
   title: string;
   desc?: string;
@@ -29,10 +64,15 @@ const FormCreateTask: React.FC<Props> = ({ title, desc }) => {
   const [modalState, setModalState] = useState<boolean>(false);
   const dispatch = useDispatch();
   const { addTask } = bindActionCreators(actionCreators, dispatch);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+    reset,
+  } = useForm<FormData>({ resolver: yupResolver(schema) });
 
-  const { register, handleSubmit, setValue, reset } = useForm<FormData>();
-
-  const onSubmit = handleSubmit((data) => createTask(data));
+  const onSubmit = handleSubmit((data: FormData) => createTask(data));
 
   const toggleModal = () => {
     setModalState(!modalState);
@@ -82,14 +122,35 @@ const FormCreateTask: React.FC<Props> = ({ title, desc }) => {
             <div className="form-control">
               <label>Title</label>
               <input {...register("title")} />
+              <div
+                className={
+                  "error-message " + (errors.title?.message ? "show" : "")
+                }
+              >
+                {errors.title?.message}
+              </div>
             </div>
             <div className="form-control">
               <label>Description</label>
               <textarea {...register("desc")}></textarea>
+              <div
+                className={
+                  "error-message " + (errors.desc?.message ? "show" : "")
+                }
+              >
+                {errors.desc?.message}
+              </div>
             </div>
             <div className="form-control">
               <label>Due</label>
               <DatetimePicker setDatetime={setDatetime} />
+              <div
+                className={
+                  "error-message " + (errors.due?.message ? "show" : "")
+                }
+              >
+                {errors.due?.message}
+              </div>
             </div>
             <div className="form-control">
               <label>Priority</label>
--- server/server.js
+++ server/server.js
@@ -81,9 +81,20 @@ function taskAdd(_, { task }) {
 
 function validateTasks({ task }) {
   const errors = [];
-
-  if (task.title.length < 3 || task.title.length > 100) {
+  if (task.title.length < 3 || task.title.length > 200) {
     errors.push("Wrong length of the title.");
+  }
+  if (task.desc.length > 2000) {
+    errors.push("Wrong length of the description.");
+  }
+  if (task.due < new Date()) {
+    errors.push("The due must be in the past.");
+  }
+  if (!["created", "expired", "done"].includes(task.status)) {
+    errors.push("The status does not have valid value.");
+  }
+  if (task.priority < 0 || task.priority > 5) {
+    errors.push("The priority must be between 1 and 5.");
   }
 
   if (errors.length) {
