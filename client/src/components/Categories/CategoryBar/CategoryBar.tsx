7bc0676c2a6f4fd9a7b1acc2ad1e6208da559c42
vojtech.tesar <vojtech.tesar@dcsoft.cz>
2022-06-08 23:30:41 +0200

    Add category - All

--- client/src/components/Categories/CategoryBar/CategoryBar.tsx
+++ client/src/components/Categories/CategoryBar/CategoryBar.tsx
@@ -6,7 +6,8 @@ import "./CategoryBar.scss";
 const CategoryBar: React.FC = () => {
   return (
     <div id="category-bar">
-      <CategoryBtn title="Home" active />
+      <CategoryBtn title="All" active />
+      <CategoryBtn title="Home" />
       <CategoryBtn title="Work" />
       <button type="button" className="btn btn-primary btn-edit-categories">
         Edit categories
